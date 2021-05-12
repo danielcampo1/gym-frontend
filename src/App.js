e4c0b5c363cbfe181621066c1b5d27592d105600
github username <github email address>
2021-05-12 16:43:20 -0500

    basic fetch working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 class App extends React.Component {
+  
+      componentDidMount() {
+        fetch("http://localhost:3000/workouts/1")
+        .then(res => res.json())
+        .then(data => console.log(data));
+      }
   render() {
+
     return (
       <div className="App">
-        
+      App
       </div>
     );
   }
