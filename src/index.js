87a51cf69b4d0661b2db0a3eb291f76c1919da87
github username <github email address>
2021-05-13 14:53:53 -0500

    imported files and added provider around app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import { provider } from 'react-redux'
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <provider>
     <App />
+    </provider>
   </React.StrictMode>,
   document.getElementById('root')
 ); 
