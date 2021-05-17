70845ac40da3c52e8933a3c84ff434e06aa51a0e
github username <github email address>
2021-05-17 11:54:26 -0500

    first GET fetch

---
+++ src/redux/actions/gymAction.js
@@ -0,0 +1,7 @@
+export const getworkout = () => {
+    return (dispatch) => {
+            fetch("http://localhost:3000/workouts")
+            .then(res => res.json())
+            .then(data => dispatch({type: "FETCH_GYM_SUCCESS", payload: data }))
+    }
+}
