6a571e8115b5f8fb8daaabe7cc269403c82f1e10
github username <github email address>
2021-05-18 17:51:43 -0500

    created workouts container

---
+++ src/components/workouts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Workouts extends React.Component {
+    render() {
+        return(
+            <div>
+                hello from worksouts
+            </div>
+        )
+    }
+}
