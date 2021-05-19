e99fa054c8ba0265c4834453104cbedb76398044
github username <github email address>
2021-05-18 20:37:23 -0500

    created file for fetch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
+import  WorkoutsForm from './components/workoutsForm'
 
 class App extends React.Component {
   
-      componentDidMount() {
-        fetch("http://localhost:3000/workouts/1")
-        .then(res => res.json())
-        .then(data => console.log(data));
-      }
+  
   render() {
 
     return (
       <div className="App">
-      App
+        <WorkoutsForm />
       </div>
     );
   }
