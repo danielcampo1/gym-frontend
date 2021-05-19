8c37f00eb1e5f31dfe4bf61ecf43aebeec60b7e1
github username <github email address>
2021-05-18 20:49:39 -0500

    created workout container

---
+++ src/containers/workoutContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import workoutsForm from '../components/workoutsForm'
+
+
+
+class WorkoutContainer extends React.Component {
+
+render() {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default WorkoutContainer
