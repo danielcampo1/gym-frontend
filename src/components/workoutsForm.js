6ce54b617149e7c1432202935707cc64a5d0ca52
github username <github email address>
2021-05-19 14:11:38 -0500

    created first imput with state and onChange

--- src/components/workoutsForm.js
+++ src/components/workoutsForm.js
@@ -2,11 +2,30 @@ import React from 'react'
 
 class WorkoutsForm extends React.Component {
 
+    state = {
+        muscle_group: "",
+        day: "",
+        name: "",
+        duration: "",
+    }
+
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value})
+    }
 
     render() {
         return(
             <div>
-                {this.props.gymWorkouts.map(workouts => <li> {workouts.plan.goal} </li>)}
+                <h1> Create a workout plan! </h1>
+                <form>
+                    <input
+                     name = 'muscle_group'
+                     onChange = {this.handleChange}
+                     value = { this.state.value}
+                    />
+                </form>
+                {/* {this.props.gymWorkouts.map(workouts => <li> {workouts.plan.goal} </li>)} */}
             </div>
         )
     }
