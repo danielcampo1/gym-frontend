e28d59cbb9aec5a9a2e6e63112f61dc420ba4aca
github username <github email address>
2021-05-18 20:36:25 -0500

    created proper file for fetch

---
+++ src/components/workoutsForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class WorkoutsForm extends React.Component {
+
+    state = {
+        muscle_group = [],
+    }
+
+    render() {
+        return(
+            <div>
+                hello from worksouts
+            </div>
+        )
+    }
+}
+
+export default WorkoutsForm
