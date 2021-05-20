f97ebe858e3b96198add2ea5f35bae1852bd451b
github username <github email address>
2021-05-19 20:15:29 -0500

    added key

--- src/components/workoutPlan.js
+++ src/components/workoutPlan.js
@@ -6,7 +6,7 @@ class WorkoutsPlan extends React.Component {
     render() {
         return(
             <div>
-                {this.props.gymWorkouts.map(workouts => <li> {workouts.plan.goal} </li>)}
+                {this.props.gymWorkouts.map(workouts => <li key={workouts.plan.id}> {workouts.plan.goal} </li>)}
             </div>
         )
     }
