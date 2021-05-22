52d97bc3c6f23781a883a085ea6a5700cfbcf86d
github username <github email address>
2021-05-22 13:16:32 -0500

    renamed

--- src/redux/reducers/gymReducer.js
+++ src/redux/reducers/gymReducer.js
@@ -1,8 +1,8 @@
 export default (state = [], action) => {
     switch (action.type) {
-      case "FETCH_GYM_SUCCESS":
+      case "FETCH_PLAN_SUCCESS":
          return action.payload;
-        case "CREATE_WORKOUT_SUCCESS":
+        case "CREATE_PLAN_SUCCESS":
          return [...state, action.payload]
       default:
         return state;
