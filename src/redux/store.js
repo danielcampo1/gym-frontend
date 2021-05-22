0e7e6b7f8b3b45c2d791823b8d039cc7b5efa541
github username <github email address>
2021-05-22 15:20:33 -0500

    took out combineReducers for now

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,11 @@
-import { createStore, combineReducers, compose, applyMiddleware } from "redux" 
+import { createStore, compose, applyMiddleware } from "redux" 
 import gymReducer from "./reducers/gymReducer";
 import thunk from "redux-thunk"
-
-const rootReducer = combineReducers({
-    gymWorkouts: gymReducer
-})
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 let store = createStore(
-    rootReducer,
+    gymReducer,
     composeEnhancers(applyMiddleware(thunk))
     );
 
