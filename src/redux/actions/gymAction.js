3633e9e61c2a8655cf9e2c63a4fc824c5f9453bc
github username <github email address>
2021-05-22 13:16:26 -0500

    renamed and added conditional

--- src/redux/actions/gymAction.js
+++ src/redux/actions/gymAction.js
@@ -1,22 +1,29 @@
-export const getWorkout = () => {
+export const getPlan = () => {
     return (dispatch) => {
-            fetch("http://localhost:3000/workouts")
+            fetch(`http://localhost:3000/plans`)
             .then(res => res.json())
-            .then(data => dispatch({type: "FETCH_GYM_SUCCESS", payload: data }))
+            .then(data => dispatch({type: "FETCH_PLAN_SUCCESS", payload: data }))
     }
 } 
 
-export const createWorkout = (newWorkoutData) => {
+export const createPlan = (newPlanData) => {
     return (dispatch) => {
-        fetch("http://localhost:3000/workouts", {
+        fetch("http://localhost:3000/plans", {
         method: "POST",
         headers: {
             Accepts: "application/json",
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ gymWorkout: newWorkoutData }),
+        body: JSON.stringify({ Plan: newPlanData }),
         })
-    .then((res) => res.json())
-    .then((data) => dispatch({ type: "CREATE_WORKOUT_SUCCESS", payload: data }))
+    // .then((res) => res.json())
+    .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(res.statusText);
+        }
+      })
+    .then((data) => dispatch({ type: "CREATE_PLAN_SUCCESS", payload: data }))
     }
 }   
