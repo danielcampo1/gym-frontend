cbc15af34a10a51835caba2e608ee6e87e463f33
github username <github email address>
2021-05-22 15:21:19 -0500

    commented out for now

--- src/components/workoutExercise.js
+++ src/components/workoutExercise.js
@@ -6,15 +6,15 @@ class WorkoutsExercise extends React.Component {
     render() {
         return(
             <div>
-                {this.props.workout.map(workouts =>  
+                {/* {this.props.workout.map(workouts =>  
                 <ul key={workouts.id}> 
                     <p> {workouts.muscle_group} </p> 
                     <p> {workouts.day} </p>
                     <p> {workouts.name}</p>
                     <p> {workouts.duration}</p>
-                </ul>
-            )   
-        }
+                </ul> */}
+            {/* )   
+        } */}
     </div>
     )
 }}
