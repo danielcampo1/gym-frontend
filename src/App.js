8fda7dd07aef70548eb4aa13171336895e6dce99
github username <github email address>
2021-05-22 15:22:01 -0500

    refactored router and switch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import  WorkoutContainer from './containers/workoutContainer'
-import home from './components/home'
-import { Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
   
@@ -10,10 +8,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <Switch>
-        <Route exact path = '/' component={home} />
-        <Route exact path='/workoutplan' component={WorkoutContainer} />
-        </Switch>
+      <WorkoutContainer />
       </div>
     );
   }
