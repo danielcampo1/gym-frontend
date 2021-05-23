dba2d4e733925078591630ed6d124a8f577d07f4
github username <github email address>
2021-05-23 11:00:08 -0500

    set up form for workouts

---
+++ src/components/workoutForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class WorkoutForm extends React.Component {
+    render() {
+        return(
+            <div>
+                hello from form
+            </div>
+        )
+    }
+}
+
+export default WorkoutForm
