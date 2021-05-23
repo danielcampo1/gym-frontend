a71a0932ec258a999b3c89382c46b967670d98de
github username <github email address>
2021-05-22 22:03:09 -0500

    seperating concern

---
+++ src/components/Plan.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Plan = (props) => {
+    console.log(props);
+
+    // let plan = props.workout[props.match.params.id - 1]
+
+    // console.log(plan);
+    return(
+        <li>
+            plan
+        </li>
+    )
+}
+
+
+export default Plan
