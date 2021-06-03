4b81e96295ba590c009a3038dcb8b20e9317216c
github username <github email address>
2021-06-03 17:49:01 -0500

    changed return of filter

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -7,8 +7,8 @@ class Plan extends React.Component {
     render() {
         console.log(this.props);
 
-        let plan = this.props.plan[this.props.match.params.id - 1]
-        // let plan = this.props.plan.filter(plan => plan.id == this.props.match.params.id)[0]
+        // let plan = this.props.plan[this.props.match.params.id - 1]
+        let plan = this.props.plan.filter(plan => plan.id == this.props.match.params.id)[0]
         console.log(plan);
         return(
             <div>
