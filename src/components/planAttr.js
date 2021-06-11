bd9b2b096165982ea59a3085a76fe870226d7985
github username <github email address>
2021-06-11 13:38:50 -0500

    added file to handle plan attr

---
+++ src/components/planAttr.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+class PlanAttr extends React.Component {
+
+    state= {
+        thumbs_up: 0
+    }
+
+    handleClick = () => {
+        this.setState({ thumbs_up: [this.props.count]})
+        this.props.reset()
+    }
+    
+    render() {
+        return(
+            <div>
+                <Link to={`/plan/${this.props.plan.id}`}> <button class="button button5"> x </button> </Link>
+                {this.props.plan.goal} <button onClick={this.handleClick} type="button" class="btn btn-outline-secondary btn-sm">{this.state.thumbs_up}  thumbs up</button>
+            </div>
+        )
+    }
+}
+
+export default PlanAttr
