9c52ac5c49be16c169675186f8786fcd7e3fc316
github username <github email address>
2021-06-11 17:16:18 -0500

    code challenge toggle darkmode

--- src/App.js
+++ src/App.js
@@ -5,12 +5,38 @@ import './App.css'
 
 
 class App extends React.Component {
+
+  state={
+    darkMode: false
+  }
+
+   darkStyle = () => {
+    this.setState({ darkMode: true })
+  }
+  
+  NonDarkMode =() => {
+    this.setState({ darkMode : false})
+  }
+
+  handleClick = () => {
+    this.setState((state) => ({darkMode: !state.darkMode}))
+  }
+    // if (this.state.darkMode === false){
+    //   return this.darkStyle()
+    // } else if (this.state.darkMode === true){
+    //   return this.NonDarkMode()
+    // }
   
   
   render() {
 
     return (
       <div className="App">
+        <div className={this.state.darkMode ? "darkmode" : null} >
+          <button onClick={this.handleClick}>
+            Dark mode
+          </button>
+        </div>
        <Navigation/> <br/>
        <PlansContainer />
       </div>
