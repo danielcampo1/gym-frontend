c434b9b0e69fefd25228926736369510117fc0e2
github username <github email address>
2021-06-11 13:40:01 -0500

    added state and reset function

--- src/components/workoutExercise.js
+++ src/components/workoutExercise.js
@@ -4,34 +4,37 @@ import WorkoutAttr from './workoutAttr'
 class WorkoutExercise extends React.Component {
 
     state={
-        count:0,
+        count: 0,
     }
 
     handleChange =(e) => { 
         this.setState({ [e.target.name] : parseInt(e.target.value) })
     }
 
-    resetState = () => this.setState({ count: 0})
+    resetState = () => this.setState({ count: ''})
 
     render() {
 
         return(
             <div>
-                 <input
+                 <input class="text-center"
                     type='number'
                      name = 'count'
                      onChange = {this.handleChange}
-                     value = {this.state.count} />
+                     value = {this.state.count}
+                     placeholder = "How Many Likes" />
 
-                 {this.props.workout && this.props.workout.map(workouts => 
+                 {this.props.workout && this.props.workout.map(workouts => {
         
-                <ul key={workouts.id}> 
+                        console.log(workouts);
+                        
+                return <ul key={workouts.id}>   
                         <WorkoutAttr text={workouts.muscle_group} count={this.state.count} reset={this.resetState} />
                         <WorkoutAttr text={workouts.day} count={this.state.count} reset={this.resetState}/>
                         <WorkoutAttr text={workouts.duration} count={this.state.count} reset={this.resetState}/>
                         <WorkoutAttr text ={workouts.name} count={this.state.count} reset={this.resetState}/>
                 </ul> 
-                 
+                 }
                 )} 
     </div>
     )
